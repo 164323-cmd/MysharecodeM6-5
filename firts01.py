0ce0f401e3e151a892cdc2d02b0235f739b85368
164323-cmd <164323@wsk.ac.th>
2026-06-16 04:50:34 +0000

    firts01.py

---
+++ firts01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวนพื้นที่ของสามเหลี่ยม")
+h=float(input("Enter hight:"))
+b=float(input("enter base :"))
+area=0.5*h*b
+print(f"ผลรวมของพื้นที่สามเหลี่ยม",area)
+print("จัดทำโดย นางสาวอุทัยวัลย์ พาลี เลขที่35")
